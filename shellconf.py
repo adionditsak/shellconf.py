71666b1f05b097aa7c22d1707cc27198196623f0
root <root@anders.mikjaer.com>
2014-03-17 14:37:03 +0000

    Made a class

--- shellconf.py
+++ shellconf.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python
 
-""" import default modules """
 import subprocess, os
-
-""" get servers """
 import servers
 
-for server in servers.servers:
-    print('----------------------------------------------')
-    print('Running configuration scripts for: ' + server)
-    print('----------------------------------------------')
+class ShellConf():
 
-    for fn in os.listdir('./scripts/'):
-        print('--------------- Running script: ' + fn + ' ---------------')
-        subprocess.call('ssh ' + server + ' "bash -s" < ./scripts/' + fn, shell=True)
-        print('')
+    def __init__():
+        pass
 
-    print('')
+    def run():
+
+        for server in servers.servers:
+            print('----------------------------------------------')
+            print('Running configuration scripts for: ' + server)
+            print('----------------------------------------------')
+
+            for fn in os.listdir('./scripts/'):
+                print('--------------- Running script: ' + fn + ' ---------------')
+                subprocess.call('ssh ' + server + ' "bash -s" < ./scripts/' + fn, shell=True)
+                print('')
+
+            print('')
+
+if __name__ == '__main__':
+    ShellConf.run()
