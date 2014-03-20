91374e76aa994b01248a2e241bb9bcd5a6878a55
root <root@anders.mikjaer.com>
2014-03-20 12:01:05 +0000

    CLI improvements

--- servers.py
+++ servers.py
@@ -3,4 +3,4 @@ List servers here in the Python list with the user@ip format.
 Make sure your host (this computer) ssh public key has been authorized.
 """
 
-servers = ['user@ip', 'user@ip']
+servers = ['user@ip-1', 'user@ip-2']
--- shellconf.py
+++ shellconf.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import subprocess, os, time, threading
+import sys, subprocess, os, time, threading
 import servers
 
 
@@ -10,25 +10,40 @@ class ShellConf():
         self.shell = shell
         self.scripts = scripts
 
-    def run(self):
+    def initiate_server_configuration(self, server):
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print(server)
+        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('')
+
+        threading.Thread(target=self.run_shell_scripts(server)).start()
+
+    def completed_server_configuration(self, server):
+        print('--- ' + server + ' completed ---\n')
+
+    def run_for_all_servers(self):
         for server in servers.servers:
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print(server)
-            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
-            print('')
-            t = threading.Thread(target=self.run_shell_scripts(server))
-            t.start()
+            self.initiate_server_configuration(server)
+
+    def run_for_one_server(self, server):
+        self.initiate_server_configuration(server)
+
+    def list_servers(self):
+        i = 0
+        print('Servers listed (./servers.py):')
+        for server in servers.servers:
+            print(str(i) + ': ' + server)
+            i = i + 1
 
     def log(self, server, script, log_input):
         with open('./log/shellconf.log', 'a') as log_file:
-            log_file.write('[' + script + ' @ ' + server + '] (' + time.strftime("%d/%m/%Y | %H:%M:%S") + '):\n' + log_input + '\n')
+            log_file.write('[' + script + ' @ ' + server + '] (' + time.strftime("%d/%m/%Y | %H:%M:%S") + '):\n' + str(log_input) + '\n')
 
     def run_shell_scripts(self, server):
         for fn in os.listdir(self.scripts):
             print('[RUNNING SCRIPT ' + self.scripts + fn + ' @ ' + server + ']')
 
             cmd = 'ssh ' + server + ' "' + self.shell + ' -s" < ' + self.scripts + fn
-
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
             output, errors = p.communicate()
 
@@ -39,7 +54,20 @@ class ShellConf():
                 self.log(server, fn, output)
                 print('- SUCCESS. See ./log/shellconf.log for details.\n')
 
+    def help(self):
+        print('Use -a/--all for all servers or -s/--server [server] for one server.')
 
 if __name__ == '__main__':
     sc = ShellConf('bash', './scripts/')
-    sc.run()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1] == '--all' or sys.argv[1] == '-a':
+            sc.run_for_all_servers()
+        elif sys.argv[1] == '--server' or sys.argv[1] == '-s':
+            if len(sys.argv) > 2:
+                sc.run_for_one_server(sys.argv[2])
+            else:
+                sc.help()
+                sc.list_servers()
+    else:
+        sc.help()
