0d13fc12d3043a5cb896ae630739f5fb60770162
Anders Aarvik <aarvik92@gmail.com>
2015-01-26 13:59:31 +0100

    Update shellconf.py

--- shellconf.py
+++ shellconf.py
@@ -6,10 +6,18 @@ shellconf.py - Simple remote configuration with shell for UNIX/Linux systems.
 :copyright: (c) 2014 Anders Aarvik
 :author: Anders Aarvik (aarvik92@gmail.com) and contributors
 :license: MIT licensed. See LICENSE.txt
+
+Requirements
+* You need an authorized ssh public key on remote servers for shellconf.py to be functional
 """
 
-import sys, subprocess, os, time, threading
-import servers
+import sys
+import subprocess
+import os
+import time
+import threading
+
+import config
 
 
 class ShellConf():
@@ -20,7 +28,7 @@ class ShellConf():
         self.lock = threading.Lock()
 
     def run_for_all_servers(self):
-        for server in servers.servers:
+        for server in config.servers:
             threading.Thread(target=lambda:self.run_shell_scripts(server)).start() # async
 
     def run_for_one_server(self, server):
@@ -28,7 +36,7 @@ class ShellConf():
 
     def list_servers(self):
         print('Servers listed (./servers.py):')
-        for i, server in enumerate(servers.servers):
+        for i, server in enumerate(config.servers):
             print('%s: %s' % (str(i), server))
 
     def log(self, server, script, log_input, log_type):
@@ -41,22 +49,27 @@ class ShellConf():
             log_file.write('[%s @ %s] (%s):\n%s\n\n' % (script, server, time.strftime("%d/%m/%Y | %H:%M:%S"), log_input))
 
     def run_shell_scripts(self, server):
-        for fn in os.listdir(self.scripts):
-            with self.lock:
-                print('[RUNNING SCRIPT %s%s @ %s]' % (self.scripts, fn, server))
-
-            cmd = 'ssh %s "%s -s" < %s%s' % (server, self.shell, self.scripts, fn)
-            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-            output, errors = p.communicate()
-
-            if p.returncode:
-                self.log(server, fn, errors, 'error')
+        for fn in config.scripts:
+            if os.path.isfile(self.scripts + fn):
                 with self.lock:
-                    print('!!! - ERRORS with %s at %s. See ./log/error.log for details.' % (fn, server))
+                    print('[RUNNING SCRIPT %s%s @ %s]' % (self.scripts, fn, server))
+
+                cmd = 'ssh %s "%s -s" < %s%s' % (server, self.shell, self.scripts, fn)
+                p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+                output, errors = p.communicate()
+
+                if p.returncode:
+                    self.log(server, fn, errors, 'error')
+                    with self.lock:
+                        print('!!! - ERRORS with %s at %s. See ./log/error.log for details.' % (fn, server))
+                else:
+                    self.log(server, fn, output, 'success')
+                    with self.lock:
+                        print('*** - SUCCESS with %s at %s. See ./log/output.log for details.' % (fn, server))
             else:
-                self.log(server, fn, output, 'success')
+                self.log(server, fn, "Script does not exist in scripts directory %s" % (self.scripts), 'error')
                 with self.lock:
-                    print('*** - SUCCESS with %s at %s. See ./log/output.log for details.' % (fn, server))
+                    print('!!! - SCRIPT %s NOT FOUND in scripts directory %s. See ./log/error.log for details.' % (fn, self.scripts))
 
         with self.lock:
             print('~~~~~~~ scripts for %s completed ~~~~~~~' % (server))
@@ -65,7 +78,7 @@ class ShellConf():
         print('Use -a/--all for all servers or -s/--server [server] for one server.')
 
 if __name__ == '__main__':
-    sc = ShellConf('bash', './scripts/')
+    sc = ShellConf(config.config[0], config.config[1])
 
     if len(sys.argv) > 1:
         if sys.argv[1] == '--all' or sys.argv[1] == '-a':
