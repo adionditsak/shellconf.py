95b8ff50ae767e54299b23a517f67e242475839b
Anders Aarvik <aarvik92@gmail.com>
2015-01-26 11:51:39 +0100

    Update servers.py

--- servers.py
+++ servers.py
@@ -3,6 +3,12 @@ List servers here in the Python list with the user@ip format.
 Make sure your host (this computer) ssh public key has been authorized.
 """
 
-servers = ['user@ip-1', 'user@ip-2']
+servers = [
+  'user@ip-1', 
+  'user@ip-2',
+  'user@ip-3',
+  'user@ip-4',
+  'user@ip-5',
+]
 
 
